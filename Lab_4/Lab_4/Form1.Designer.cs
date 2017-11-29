30fc995ca8ea5c682c04c3e9cd4edd9e7bbb8cdb
Prudnichenkov__Daniil <octaneione@gmail.com>
2017-11-29 16:11:53 +0300

    Add files via upload

---
+++ Lab_4/Lab_4/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Lab_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadb = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.findb = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // loadb
+            // 
+            this.loadb.Location = new System.Drawing.Point(28, 12);
+            this.loadb.Name = "loadb";
+            this.loadb.Size = new System.Drawing.Size(75, 23);
+            this.loadb.TabIndex = 0;
+            this.loadb.Text = "Load Data";
+            this.loadb.UseVisualStyleBackColor = true;
+            this.loadb.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(28, 48);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(115, 264);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(173, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(173, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(169, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 4;
+            // 
+            // findb
+            // 
+            this.findb.Location = new System.Drawing.Point(311, 236);
+            this.findb.Name = "findb";
+            this.findb.Size = new System.Drawing.Size(75, 23);
+            this.findb.TabIndex = 5;
+            this.findb.Text = "Find word";
+            this.findb.UseVisualStyleBackColor = true;
+            this.findb.Click += new System.EventHandler(this.findb_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(176, 236);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 331);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.findb);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.loadb);
+            this.Name = "Form1";
+            this.Text = "Lab_4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadb;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button findb;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Lab_4/Lab_4/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_4
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch cl = new Stopwatch();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cl.Reset();
+            OpenFileDialog win1 = new OpenFileDialog();
+            win1.InitialDirectory = "\\Mac/Home/Documents/Course_2\bkIT/Lab_4";
+            win1.Filter = "txt files (*.txt)|*.txt";//|All files (*.*)|*.*";
+            win1.FilterIndex = 2;
+            win1.RestoreDirectory = true;
+            if (win1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    cl.Start();
+                    string buf = File.ReadAllText(win1.FileName);
+                    List<string> a = new List<string>();
+                    string[] buf2 = buf.Split();
+                    foreach (string l in buf2)
+                    {
+                        if (!a.Contains(l))
+                            a.Add(l);
+                    }
+                    a.Sort();
+                    cl.Stop();
+                    label1.Text = "Opening file, reading and sorting array(ms): "+cl.ElapsedMilliseconds.ToString();
+                    addToListBox(a);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("ERROR! Couldn't read file from disk!\n Original error: " + ex.Message);
+                }
+            }
+        }
+        void addToListBox(List<string> a)
+        {
+
+            listBox1.BeginUpdate();
+            foreach (string l in a)
+            {
+                listBox1.Items.Add(l);
+            }
+            listBox1.EndUpdate();
+         }
+        private void findb_Click(object sender, EventArgs e)
+        {
+            cl.Reset();
+            cl.Start();
+            listBox1.SelectedIndex = listBox1.FindStringExact(textBox1.Text);
+            cl.Stop();
+            label2.Text = "Searching in ListBox(ms): " + cl.ElapsedMilliseconds.ToString();
+        }
+    }
+}
